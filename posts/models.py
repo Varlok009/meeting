ee4b5f9c65565390497dffb1b83c9ea41247f5ce
Varlok009 <varlok009@mail.ru>
2022-06-27 08:48:06 +0300

    add comments for post

--- posts/models.py
+++ posts/models.py
@@ -15,8 +15,8 @@ class Group(models.Model):
 
 class Post(models.Model):
     text = models.TextField(verbose_name='Текст сообщения')
-    pub_date = models.DateTimeField("date published", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     group = models.ForeignKey(
         Group,
         on_delete=models.CASCADE,
@@ -29,3 +29,9 @@ class Post(models.Model):
     def __str__(self):
         return self.text
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField(verbose_name='Текст сообщения')
+    created = models.DateTimeField('date published', auto_now_add=True)
