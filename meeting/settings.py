23510d27447364c4c93a6020ab115e5792a5fca1
Varlok009 <varlok009@mail.ru>
2022-07-01 16:14:29 +0300

    add postgres in docker

--- meeting/settings.py
+++ meeting/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'meeting.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'meeting',
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "meeting_db",
+        "PORT": 5432,
     }
 }
 
